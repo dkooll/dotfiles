9adf6d9c8ee6b82b61a84d190ca0880e5e15694d
dkool <dkool@dkools-MacBook-Pro.local>
2024-12-16 14:39:09 +0100

    remove comments diffview plugin

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -7,36 +7,3 @@ return {
     require("diffview").setup({})
   end,
 }
-
-
---return {
-  --"sindrets/diffview.nvim",
-  --lazy = true,
-  --keys = {
-    --{ "<leader>gd", "<cmd>DiffviewFileHistory<CR>", desc = "Diffview: Git Diff View File History" },
-  --},
-  --config = function()
-    --require("diffview").setup({
-      --view = {
-        --default = {
-          --layout = "diff2_horizontal",
-          --disable_diagnostics = false,
-        --},
-      --},
-    --})
-  --end,
---}
-
-
---return {
-  --"sindrets/diffview.nvim",
-  --lazy = true,
-  --keys = {
-    --{ "<leader>gd", "<cmd>DiffviewFileHistory<CR>", desc = "Diffview: Git Diff Vieuw File History" },
-  --},
---}
-
-
---return {
-  --"sindrets/diffview.nvim",
---}
