e24a8276887b5c6dd9a374c50b4820feab6a3863
dkool <dkool@dkools-MacBook-Pro.local>
2024-12-20 10:14:19 +0100

    removed some redundant nvim key bindings

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -9,23 +9,13 @@ local navigation_keys = {
 
 local map = vim.keymap.set
 
--- TODO: add different keymap
-
--- Keep cursor centered when scrolling
---vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
---vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
-
 -- Move selected line / block of text in visual mode
 vim.keymap.set("v", "<S-k>", ":m '<-2<CR>gv=gv", { silent = true })
 vim.keymap.set("v", "<S-j>", ":m '>+1<CR>gv=gv", { silent = true })
 
--- map("n", "<C-Space>", ":WhichKey \\<space><cr>", opts)
+-- scrolls up and down half a page centered
 map("n", "<C-d>", "<C-d>zz", opts)
 map("n", "<C-u>", "<C-u>zz", opts)
-map("v", "J", ":m '>+1<CR>gv=gv", opts)
-map("v", "K", ":m '<-2<CR>gv=gv", opts)
-map("n", "<leader><leader>", ":Telescope buffers<CR>", opts)
-map("n", "m", "ciw", opts)
 
 -- Remap for dealing with visual line wraps
 map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
@@ -39,13 +29,15 @@ map("v", ">", ">gv")
 map("v", "p", '"_dp')
 map("v", "P", '"_dP')
 
--- Fast saving
 vim.keymap.set('n', 'w', ':write!<CR>')
 vim.keymap.set('n', 'q', ':q!<CR>', { silent = true })
+map("n", "m", "ciw", opts)
 
--- Exit on jj and jk
-vim.keymap.set('i', 'jj', '<ESC>')
-vim.keymap.set('i', 'jk', '<ESC>')
+-- Left Option/Alt + numbers for percentage-based window sizing vertically
+map("n", "¡", "<cmd>vertical resize 90<CR>", opts)     -- Option + 1 -> 25%
+map("n", "€", "<cmd>vertical resize 135<CR>", opts)    -- Option + 2 -> 35%
+map("n", "£", "<cmd>vertical resize 180<CR>", opts)    -- Option + 3 -> 50%
+map("n", "¢", "<cmd>vertical resize 360<CR>", opts)    -- Option + 4 -> 100%
 
 for key, cmd in pairs(navigation_keys) do
   -- Navigate between windows in Normal mode
