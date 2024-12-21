7afdf64e9887c0cabab1c2ce7c9ce757558d5fe7
dkool <dkool@dkools-MacBook-Pro.local>
2024-12-21 12:14:05 +0100

    small refactor mason lua file

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -8,34 +8,52 @@ return {
       "neovim/nvim-lspconfig",
       "hrsh7th/cmp-nvim-lsp",
       "b0o/schemastore.nvim",
+      "nvim-telescope/telescope.nvim",
+      "folke/trouble.nvim",
     },
     config = function()
-      -- Basic mason setup
-      require("mason").setup({
-        ui = { border = "rounded", icons = { package_installed = "✓", package_pending = "➜", package_uninstalled = "✗" } }
+      -- Import required modules
+      local mason = require("mason")
+      local mason_lspconfig = require("mason-lspconfig")
+      local mason_tool_installer = require("mason-tool-installer")
+      local lspconfig = require("lspconfig")
+      local cmp_nvim_lsp = require("cmp_nvim_lsp")
+      local schemastore = require("schemastore")
+
+      -- Setup capabilities once
+      local capabilities = cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+      -- Basic mason setup with consistent UI
+      mason.setup({
+        ui = {
+          border = "rounded",
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+          }
+        }
       })
 
-      -- Load and configure LSP immediately
-      local lspconfig = require("lspconfig")
-      local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
-
-      -- Server configurations
+      -- Define LSP servers configuration
       local servers = {
+        -- Servers with custom settings
         lua_ls = {
           settings = {
             Lua = {
               runtime = { version = "LuaJIT" },
-              workspace = { checkThirdParty = false, library = vim.api.nvim_get_runtime_file("", true) },
+              workspace = {
+                checkThirdParty = false,
+                library = vim.api.nvim_get_runtime_file("", true)
+              },
               completion = { callSnippet = "Replace" }
             }
           }
         },
         terraformls = {
-          cmd = { vim.fn.expand('~/.local/share/nvim/mason/bin/terraform-ls'), "serve" },
+          cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/terraform-ls"), "serve" },
           filetypes = { "terraform", "tf", "terraform-vars" },
-          root_dir = function(fname)
-            return require('lspconfig.util').root_pattern('.terraform', '.git')(fname) or vim.fs.dirname(fname)
-          end
+          root_dir = lspconfig.util.root_pattern(".terraform", ".git") or vim.fs.dirname
         },
         yamlls = {
           settings = {
@@ -51,41 +69,41 @@ return {
         jsonls = {
           settings = {
             json = {
-              schemas = require("schemastore").json.schemas(),
+              schemas = schemastore.json.schemas(),
               validate = { enable = true }
             }
           }
-        }
+        },
+        -- Servers with default settings
+        bashls = {},
+        vimls = {},
+        pyright = {},
+        solidity_ls_nomicfoundation = {},
       }
 
-      -- Setup all configured servers
+      -- Ensure servers are installed and configured
+      mason_lspconfig.setup({
+        ensure_installed = vim.tbl_keys(servers),
+        automatic_installation = true,
+      })
+
+      -- Setup all servers with their configurations
       for server_name, server_settings in pairs(servers) do
         lspconfig[server_name].setup(vim.tbl_deep_extend("force", server_settings, { capabilities = capabilities }))
       end
 
-      -- Setup basic servers
-      for _, server in ipairs({ "bashls", "vimls", "pyright", "solidity_ls_nomicfoundation" }) do
-        lspconfig[server].setup({ capabilities = capabilities })
-      end
+      -- Install additional tools
+      mason_tool_installer.setup({
+        ensure_installed = {
+          "prettier",
+          "stylua",
+          "terraform-ls",
+          "tflint",
+          "tfsec",
+        }
+      })
 
-      -- Defer the mason-lspconfig and tool installer setup
-      vim.defer_fn(function()
-        require("mason-lspconfig").setup({
-          ensure_installed = {
-            "jsonls", "terraformls", "lua_ls", "bashls",
-            "vimls", "pyright", "solidity_ls_nomicfoundation", "yamlls",
-          },
-          automatic_installation = true
-        })
-
-        require("mason-tool-installer").setup({
-          ensure_installed = {
-            "prettier", "stylua", "terraform-ls", "tflint", "tfsec",
-          }
-        })
-      end, 0)
-
-      -- Set up diagnostic config and signs
+      -- Configure diagnostics
       vim.diagnostic.config({
         virtual_text = true,
         signs = true,
@@ -98,175 +116,198 @@ return {
           border = "rounded",
           source = "if_many",
           header = "",
-          prefix = ""
+          prefix = "",
         }
       })
 
-      for type, icon in pairs({ Error = "󰊨", Warn = "󰝦", Hint = "󰈧", Info = "󰉉" }) do
+      -- Set diagnostic icons
+      local signs = {
+        Error = "󰊨",
+        Warn = "󰝦",
+        Hint = "󰈧",
+        Info = "󰉉"
+      }
+
+      for type, icon in pairs(signs) do
         local hl = "DiagnosticSign" .. type
         vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
       end
     end,
     keys = {
-      { "gd",          function() vim.lsp.buf.definition() end },
-      { "K",           function() vim.lsp.buf.hover() end },
-      { "<leader>vws", function() vim.lsp.buf.workspace_symbol() end },
-      { "<leader>vd",  function() vim.diagnostic.open_float() end },
-      { "[d",          function() vim.diagnostic.goto_prev() end },
-      { "]d",          function() vim.diagnostic.goto_next() end },
-      { "<leader>vca", function() vim.lsp.buf.code_action() end },
-      { "<leader>vrr", function() vim.lsp.buf.references() end },
-      { "<leader>vrn", function() vim.lsp.buf.rename() end },
-      { "<C-h>",       function() vim.lsp.buf.signature_help() end, mode = "i" },
+      -- LSP Navigation
+      { "gd",         vim.lsp.buf.definition,                                                        desc = "Go to definition" },
+      { "gD",         vim.lsp.buf.declaration,                                                       desc = "Go to declaration" },
+      { "gi",         vim.lsp.buf.implementation,                                                    desc = "Go to implementation" },
+      { "go",         vim.lsp.buf.type_definition,                                                   desc = "Go to type definition" },
+      { "gr",         vim.lsp.buf.references,                                                        desc = "Show references" },
+      { "K",          vim.lsp.buf.hover,                                                             desc = "Show hover documentation" },
+      { "gl",         vim.diagnostic.open_float,                                                     desc = "Show line diagnostics" },
+
+      -- LSP Actions
+      { "<leader>la", vim.lsp.buf.code_action,                                                       desc = "Code actions" },
+      { "<leader>lA", vim.lsp.buf.range_code_action,                                                 mode = "v",                              desc = "Range code actions" },
+      { "<leader>lf", vim.lsp.buf.format,                                                            desc = "Format code" },
+      { "<leader>lr", vim.lsp.buf.rename,                                                            desc = "Rename symbol" },
+      { "<leader>ls", vim.lsp.buf.signature_help,                                                    desc = "Signature help" },
+
+      -- Telescope Integration
+      { "<leader>lR", "<cmd>Telescope lsp_references<cr>",                                           desc = "Show references (Telescope)" },
+      { "<leader>lw", "<cmd>Telescope diagnostics<cr>",                                              desc = "Show diagnostics (Telescope)" },
+      { "<leader>lt", [[<Esc><Cmd>lua require('telescope').extensions.refactoring.refactors()<CR>]], desc = "Refactoring options" },
+
+      -- Trouble Integration
+      { "<leader>ll", "<cmd>TroubleToggle document_diagnostics<cr>",                                 desc = "Document diagnostics (Trouble)" },
+      { "<leader>lL", "<cmd>TroubleToggle workspace_diagnostics<cr>",                                desc = "Workspace diagnostics (Trouble)" },
+
+      -- Insert Mode Bindings
+      { "<C-h>",      vim.lsp.buf.signature_help,                                                    mode = "i",                              desc = "Signature help" },
     }
   }
 }
---return {
-  --{
-    --"williamboman/mason.nvim",
-    --event = "VeryLazy",
-    --dependencies = {
-      --"williamboman/mason-lspconfig.nvim",
-      --"WhoIsSethDaniel/mason-tool-installer.nvim",
-      --"neovim/nvim-lspconfig",
-      --"hrsh7th/cmp-nvim-lsp",
-      --"b0o/schemastore.nvim",
-    --},
-    --config = function()
-      ---- Cache frequently used modules
-      --local mason = require("mason")
-      --local mason_lspconfig = require("mason-lspconfig")
-      --local mason_tool_installer = require("mason-tool-installer")
-      --local lspconfig = require("lspconfig")
-      --local cmp_nvim_lsp = require("cmp_nvim_lsp")
-      --local capabilities = cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-      ---- Basic mason setup
-      --mason.setup({
-        --ui = { border = "rounded", icons = { package_installed = "✓", package_pending = "➜", package_uninstalled = "✗" } }
-      --})
 
-      --local servers = {
-        --["lua_ls"] = {
-          --settings = {
-            --Lua = {
-              --runtime = { version = "LuaJIT" },
-              --workspace = { checkThirdParty = false, library = vim.api.nvim_get_runtime_file("", true) },
-              --completion = { callSnippet = "Replace" }
-            --}
-          --}
-        --},
-        --["terraformls"] = {
-          --cmd = { vim.fn.expand('~/.local/share/nvim/mason/bin/terraform-ls'), "serve" },
-          --filetypes = { "terraform", "tf", "terraform-vars" },
-          --root_dir = function(fname)
-            --return require('lspconfig.util').root_pattern('.terraform', '.git')(fname) or vim.fs.dirname(fname)
-          --end
-        --},
-        --["yamlls"] = {
-          --settings = {
-            --yaml = {
-              --schemas = {
-                --["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
-                --["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] =
-                --"docker-compose*.yml"
-              --}
-            --}
-          --}
-        --},
-        --["jsonls"] = {
-          --settings = {
-            --json = {
-              --schemas = require("schemastore").json.schemas(),
-              --validate = { enable = true }
-            --}
-          --}
-        --}
-      --}
-
-      --mason_lspconfig.setup({
-        --ensure_installed = {
-          --"jsonls",
-          --"terraformls",
-          --"lua_ls",
-          --"bashls",
-          --"vimls",
-          --"pyright",
-          --"solidity_ls_nomicfoundation",
-          --"yamlls",
-        --},
-        --automatic_installation = true
-      --})
-
-      --mason_tool_installer.setup({
-        --ensure_installed = {
-          --"prettier",
-          --"stylua",
-          --"terraform-ls",
-          --"tflint",
-          --"tfsec",
-        --}
-      --})
-
-      ---- Setup all servers
-      --for server_name, server_settings in pairs(servers) do
-        --local config = vim.tbl_deep_extend("force", server_settings, { capabilities = capabilities })
-        --lspconfig[server_name].setup(config)
-      --end
-
-      ---- Setup basic servers
-      --for _, server in ipairs({ "bashls", "vimls", "pyright", "solidity_ls_nomicfoundation" }) do
-        --lspconfig[server].setup({ capabilities = capabilities })
-      --end
-
-      ---- set lsp keymaps only once
-      --local function setup_keymaps(bufnr)
-        --local opts = { buffer = bufnr, silent = true }
-        --local keymaps = {
-          --{ "n", "gd",          vim.lsp.buf.definition },
-          --{ "n", "K",           vim.lsp.buf.hover },
-          --{ "n", "<leader>vws", vim.lsp.buf.workspace_symbol },
-          --{ "n", "<leader>vd",  vim.diagnostic.open_float },
-          --{ "n", "[d",          vim.diagnostic.goto_prev },
-          --{ "n", "]d",          vim.diagnostic.goto_next },
-          --{ "n", "<leader>vca", vim.lsp.buf.code_action },
-          --{ "n", "<leader>vrr", vim.lsp.buf.references },
-          --{ "n", "<leader>vrn", vim.lsp.buf.rename },
-          --{ "i", "<C-h>",       vim.lsp.buf.signature_help }
-        --}
-        --for _, map in ipairs(keymaps) do
-          --vim.keymap.set(map[1], map[2], map[3], opts)
-        --end
-      --end
-
-      ---- attach keymaps to lsp buffers
-      --vim.api.nvim_create_autocmd("LspAttach", {
-        --group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-        --callback = function(ev)
-          --setup_keymaps(ev.buf)
-        --end
-      --})
-
-      --vim.diagnostic.config({
-        --virtual_text = true,
-        --signs = true,
-        --update_in_insert = false,
-        --underline = true,
-        --severity_sort = true,
-        --float = {
-          --focusable = false,
-          --style = "minimal",
-          --border = "rounded",
-          --source = "if_many",
-          --header = "",
-          --prefix = ""
-        --}
-      --})
-
-      --for type, icon in pairs({ Error = "󰊨", Warn = "󰝦", Hint = "󰈧", Info = "󰉉" }) do
-        --local hl = "DiagnosticSign" .. type
-        --vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-      --end
-    --end
-  --}
---}
+-- return {
+--   {
+--     "williamboman/mason.nvim",
+--     event = { "BufReadPre", "BufNewFile" },
+--     dependencies = {
+--       "williamboman/mason-lspconfig.nvim",
+--       "WhoIsSethDaniel/mason-tool-installer.nvim",
+--       "neovim/nvim-lspconfig",
+--       "hrsh7th/cmp-nvim-lsp",
+--       "b0o/schemastore.nvim",
+--       "nvim-telescope/telescope.nvim",
+--       "folke/trouble.nvim",
+--     },
+--     config = function()
+--       -- Basic mason setup
+--       require("mason").setup({
+--         ui = { border = "rounded", icons = { package_installed = "✓", package_pending = "➜", package_uninstalled = "✗" } }
+--       })
+--
+--       -- Load and configure LSP immediately
+--       local lspconfig = require("lspconfig")
+--       local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+--
+--       -- Server configurations
+--       local servers = {
+--         lua_ls = {
+--           settings = {
+--             Lua = {
+--               runtime = { version = "LuaJIT" },
+--               workspace = { checkThirdParty = false, library = vim.api.nvim_get_runtime_file("", true) },
+--               completion = { callSnippet = "Replace" }
+--             }
+--           }
+--         },
+--         terraformls = {
+--           cmd = { vim.fn.expand('~/.local/share/nvim/mason/bin/terraform-ls'), "serve" },
+--           filetypes = { "terraform", "tf", "terraform-vars" },
+--           root_dir = function(fname)
+--             return require('lspconfig.util').root_pattern('.terraform', '.git')(fname) or vim.fs.dirname(fname)
+--           end
+--         },
+--         yamlls = {
+--           settings = {
+--             yaml = {
+--               schemas = {
+--                 ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+--                 ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] =
+--                 "docker-compose*.yml"
+--               }
+--             }
+--           }
+--         },
+--         jsonls = {
+--           settings = {
+--             json = {
+--               schemas = require("schemastore").json.schemas(),
+--               validate = { enable = true }
+--             }
+--           }
+--         }
+--       }
+--
+--       -- Setup all configured servers
+--       for server_name, server_settings in pairs(servers) do
+--         lspconfig[server_name].setup(vim.tbl_deep_extend("force", server_settings, { capabilities = capabilities }))
+--       end
+--
+--       -- Setup basic servers
+--       for _, server in ipairs({ "bashls", "vimls", "pyright", "solidity_ls_nomicfoundation" }) do
+--         lspconfig[server].setup({ capabilities = capabilities })
+--       end
+--
+--       -- Rest of your mason setup...
+--       vim.defer_fn(function()
+--         require("mason-lspconfig").setup({
+--           ensure_installed = {
+--             "jsonls", "terraformls", "lua_ls", "bashls",
+--             "vimls", "pyright", "solidity_ls_nomicfoundation", "yamlls",
+--           },
+--           automatic_installation = true
+--         })
+--
+--         require("mason-tool-installer").setup({
+--           ensure_installed = {
+--             "prettier", "stylua", "terraform-ls", "tflint", "tfsec",
+--           }
+--         })
+--       end, 0)
+--
+--       -- Diagnostic configuration
+--       vim.diagnostic.config({
+--         virtual_text = true,
+--         signs = true,
+--         update_in_insert = false,
+--         underline = true,
+--         severity_sort = true,
+--         float = {
+--           focusable = false,
+--           style = "minimal",
+--           border = "rounded",
+--           source = "if_many",
+--           header = "",
+--           prefix = ""
+--         }
+--       })
+--
+--       for type, icon in pairs({ Error = "󰊨", Warn = "󰝦", Hint = "󰈧", Info = "󰉉" }) do
+--         local hl = "DiagnosticSign" .. type
+--         vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+--       end
+--     end,
+--     keys = {
+--       -- Non-leader keymaps
+--       { "gd",         function() vim.lsp.buf.definition() end },
+--       { "gD",         function() vim.lsp.buf.declaration() end },
+--       { "gi",         function() vim.lsp.buf.implementation() end },
+--       { "gl",         function() vim.diagnostic.open_float() end },
+--       { "go",         function() vim.lsp.buf.type_definition() end },
+--       { "gr",         function() vim.lsp.buf.references() end },
+--       { "K",          function() vim.lsp.buf.hover() end },
+--
+--       -- Leader keymaps
+--       { "<leader>la", function() vim.lsp.buf.code_action() end },
+--       { "<leader>lA", function() vim.lsp.buf.range_code_action() end, mode = "v" },
+--       { "<leader>lf", function() vim.lsp.buf.format() end },
+--       { "<leader>lr", function() vim.lsp.buf.rename() end },
+--       { "<leader>ls", function() vim.lsp.buf.signature_help() end },
+--       { "<leader>lR", "<cmd>Telescope lsp_references<cr>" },
+--       { "<leader>ll", "<cmd>TroubleToggle document_diagnostics<cr>" },
+--       { "<leader>lL", "<cmd>TroubleToggle workspace_diagnostics<cr>" },
+--       { "<leader>lw", "<cmd>Telescope diagnostics<cr>" },
+--       { "<leader>lt", [[<Esc><Cmd>lua require('telescope').extensions.refactoring.refactors()<CR>]] },
+--
+--       -- Insert mode bindings
+--       { "<C-h>",      function() vim.lsp.buf.signature_help() end, mode = "i" },
+--     }
+--   }
+-- }
+--
+--
+--
+--
+--
+--
+--
