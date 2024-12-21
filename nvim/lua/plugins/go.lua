abe7375723e597a971d328b14fda3c45193ab9a8
dkool <dkool@dkools-MacBook-Pro.local>
2024-12-21 12:15:48 +0100

    moved rust and go specific keybindings to the plugin file itself

--- nvim/lua/plugins/go.lua
+++ nvim/lua/plugins/go.lua
@@ -39,6 +39,43 @@ return {
         diagnosticStyle = "instant",
       },
     },
+    keys = {
+      -- LSP format
+      { "<leader>lf",  function() vim.lsp.buf.format({ async = true }) end, desc = "Format file" },
+
+      -- Main Go commands
+      -- { "<leader>gi",  "<cmd>GoInstallDeps<cr>",                            desc = "Install Go Dependencies" },
+      -- { "<leader>go",  "<cmd>GoPkgOutline<cr>",                             desc = "Outline" },
+      -- { "<leader>gI",  "<cmd>GoToggleInlay<cr>",                            desc = "Toggle inlay" },
+      -- { "<leader>gg",  "<cmd>GoGenerate<cr>",                               desc = "Go Generate" },
+      -- { "<leader>gG",  "<cmd>GoGenerate %<cr>",                             desc = "Go Generate File" },
+      -- { "<leader>gc",  "<cmd>GoCmt<cr>",                                    desc = "Generate Comment" },
+      -- { "<leader>gs",  "<cmd>GoFillStruct<cr>",                             desc = "Autofill struct" },
+      -- { "<leader>gj",  "<cmd>'<,'>GoJson2Struct<cr>",                       desc = "Json to struct" },
+
+      -- Helper submenu
+      -- { "<leader>gha", "<cmd>GoAddTag<cr>",                                 desc = "Add tags to struct" },
+      -- { "<leader>ghr", "<cmd>GoRMTag<cr>",                                  desc = "Remove tags to struct" },
+      -- { "<leader>ghc", "<cmd>GoCoverage<cr>",                               desc = "Test coverage" },
+      -- { "<leader>ghv", "<cmd>GoVet<cr>",                                    desc = "Go vet" },
+      -- { "<leader>ght", "<cmd>GoModTidy<cr>",                                desc = "Go mod tidy" },
+      -- { "<leader>ghi", "<cmd>GoModInit<cr>",                                desc = "Go mod init" },
+
+      -- Tests submenu
+      -- { "<leader>gtr", "<cmd>GoTest<cr>",                                   desc = "Run tests" },
+      -- { "<leader>gta", "<cmd>GoAlt!<cr>",                                   desc = "Open alt file" },
+      -- { "<leader>gts", "<cmd>GoAltS!<cr>",                                  desc = "Open alt file in split" },
+      -- { "<leader>gtv", "<cmd>GoAltV!<cr>",                                  desc = "Open alt file in vertical split" },
+      -- { "<leader>gtu", "<cmd>GoTestFunc<cr>",                               desc = "Run test for current func" },
+      -- { "<leader>gtf", "<cmd>GoTestFile<cr>",                               desc = "Run test for current file" },
+      -- { "<leader>gtT", "<cmd>GoTestAdd<cr>",                                desc = "Add Test" },
+      -- { "<leader>gtA", "<cmd>GoTestsAll<cr>",                               desc = "Add All Tests" },
+      -- { "<leader>gte", "<cmd>GoTestsExp<cr>",                               desc = "Add Exported Tests" },
+
+      -- Code Lens submenu
+      -- { "<leader>gxl", "<cmd>GoCodeLenAct<cr>",                             desc = "Toggle Lens" },
+      -- { "<leader>gxa", "<cmd>GoCodeAction<cr>",                             desc = "Code Action" },
+    },
     config = function(_, opts)
       require("go").setup(opts)
     end,
--- nvim/lua/plugins/rust.lua
+++ nvim/lua/plugins/rust.lua
@@ -6,6 +6,7 @@ return {
       "neovim/nvim-lspconfig",
       "nvim-lua/plenary.nvim",
       "mfussenegger/nvim-dap",
+      "saecki/crates.nvim",
     },
     opts = {
       server = {
@@ -57,6 +58,39 @@ return {
           auto_focus = false,
         },
       },
+    },
+    keys = {
+      -- Rust Tools
+      -- { "<leader>Rj", function() require('rust-tools').join_lines.join_lines() end, desc = "Join Lines" },
+      -- { "<leader>Rr", "<cmd>RustRunnables<cr>",                                     desc = "Runnables" },
+      -- { "<leader>Rt", "<cmd>lua *CARGO*TEST()<cr>",                                 desc = "Cargo Test" },
+      -- { "<leader>Rm", "<cmd>RustExpandMacro<cr>",                                   desc = "Expand Macro" },
+      -- { "<leader>Rc", "<cmd>RustOpenCargo<cr>",                                     desc = "Open Cargo" },
+      -- { "<leader>Rp", "<cmd>RustParentModule<cr>",                                  desc = "Parent Module" },
+      -- { "<leader>Rd", "<cmd>RustDebuggables<cr>",                                   desc = "Debuggables" },
+      -- { "<leader>Rv", "<cmd>RustViewCrateGraph<cr>",                                desc = "View Crate Graph" },
+      {
+        "<leader>RR",
+        function()
+          require('rust-tools/workspace_refresh')._reload_workspace_from_cargo_toml()
+        end,
+        desc = "Rust: Reload Workspace"
+      },
+
+      -- Crates
+      -- { "<leader>Co", function() require('crates').show_popup() end,              desc = "Show popup" },
+      -- { "<leader>Cr", function() require('crates').reload() end,                  desc = "Reload" },
+      -- { "<leader>Cv", function() require('crates').show_versions_popup() end,     desc = "Show Versions" },
+      -- { "<leader>Cf", function() require('crates').show_features_popup() end,     desc = "Show Features" },
+      -- { "<leader>Cd", function() require('crates').show_dependencies_popup() end, desc = "Show Dependencies Popup" },
+      -- { "<leader>Cu", function() require('crates').update_crate() end,            desc = "Update Crate" },
+      -- { "<leader>Ca", function() require('crates').update_all_crates() end,       desc = "Update All Crates" },
+      -- { "<leader>CU", function() require('crates').upgrade_crate() end,           desc = "Upgrade Crate" },
+      -- { "<leader>CA", function() require('crates').upgrade_all_crates(true) end,  desc = "Upgrade All Crates" },
+      -- { "<leader>CH", function() require('crates').open_homepage() end,           desc = "Open Homepage" },
+      -- { "<leader>CR", function() require('crates').open_repository() end,         desc = "Open Repository" },
+      -- { "<leader>CD", function() require('crates').open_documentation() end,      desc = "Open Documentation" },
+      -- { "<leader>CC", function() require('crates').open_crates_io() end,          desc = "Open Crate.io" },
     },
     config = function(_, opts)
       require("rust-tools").setup(opts)
