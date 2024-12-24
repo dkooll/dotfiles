a44ff73cf930d2bf34e73262b7d7035fc6d66ec6
dkool <dkool@dkools-MacBook-Pro.local>
2024-12-24 10:54:24 +0100

    cleanup tmuxer nvim config abit more

--- nvim/lua/plugins/tmuxer-nvim.lua
+++ nvim/lua/plugins/tmuxer-nvim.lua
@@ -11,33 +11,12 @@ return {
       end,
       desc = "Tmuxer: Create Tmux Session"
     },
-    { "<leader>ts", function() require("tmuxer").tmux_sessions() end, desc = "Tmuxer: Switch Tmux Session" },
+    {
+      "<leader>ts",
+      function()
+        require("tmuxer").tmux_sessions()
+      end,
+      desc = "Tmuxer: Switch Tmux Session"
+    },
   },
 }
-
---
---
---
--- return {
---   "dkooll/tmuxer.nvim",
---   dependencies = { "nvim-telescope/telescope.nvim" },
---   cmd = { "WorkspaceOpen", "TmuxSessions" },
---   keys = {
---     { "<leader>tc", "<cmd>WorkspaceOpen<cr>", desc = "Tmuxer: Create Tmux Session" },
---     { "<leader>ts", "<cmd>TmuxSessions<cr>",  desc = "Tmuxer: Switch Tmux Session" },
---   },
---   lazy = true,
---   opts = {
---     workspaces = {
---       { name = "workspaces", path = "~/Documents/workspaces" },
---     }
---   },
---   config = function(_, opts)
---     local tmuxer = require("tmuxer")
---     tmuxer.setup(opts)
---     vim.api.nvim_create_user_command("WorkspaceOpen", function()
---       tmuxer.open_workspace_popup(tmuxer.workspaces[1])
---     end, {})
---     vim.api.nvim_create_user_command("TmuxSessions", tmuxer.tmux_sessions, {})
---   end,
--- }
