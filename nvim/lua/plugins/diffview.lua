5f800b6975d1da3cee48aaa197cd9eee1e92c128
dkool <dkool@dkools-MacBook-Pro.local>
2025-01-06 10:27:38 +0100

    merge git related nvim plugins into one file

--- nvim/lua/plugins/diffview.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "sindrets/diffview.nvim",
-  keys = {
-    { "<leader>gh", "<cmd>DiffviewFileHistory<CR>", desc = "Diffview: Git Diff View File History" },
-  },
-  config = function()
-    require("diffview").setup({})
-  end,
-}
