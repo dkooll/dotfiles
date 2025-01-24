9efe8972626f7b05f74fe40bee1e0bdef8d089c4
dkool <dennis.kool@cloudnation.nl>
2025-01-24 12:44:40 +0100

    fix formatting

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,8 +1,8 @@
 local opts = { noremap = true, silent = true }
 
 -- Change window layout
-vim.keymap.set('n', ',h', '<C-w>H', opts)  -- ,h moves window left
-vim.keymap.set('n', ',l', '<C-w>L', opts)  -- ,l moves window right
+vim.keymap.set('n', ',h', '<C-w>H', opts) -- ,h moves window left
+vim.keymap.set('n', ',l', '<C-w>L', opts) -- ,l moves window right
 
 -- Window movement with Ctrl+w followed by arrows
 vim.keymap.set('n', '<C-w><Left>', '<C-w>h', opts)
